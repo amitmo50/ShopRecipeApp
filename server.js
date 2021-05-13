56119bfbe03acaaef6042ad94dddf08ef313ff31
Amit Mor <amitmo50@gmail.com>
2021-05-13 12:57:14 +0300

    Amit: Updated Server.js path to deployment

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const compression = require('compression')
 const app = express();
 app.use(compression());
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ironsource-assignment'));
+app.use(express.static('./dist/ShopRecipeApp'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/dist/ironsource-assignment/index.html'))
+    res.sendFile(path.join(__dirname+'/dist/ShopRecipeApp/index.html'))
 });
 
 // Start the app by listening on the default Heroku port
